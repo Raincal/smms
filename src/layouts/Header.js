0cefc11d88afaffb961bcc3fc84d4ece409da312
Raincal <cyj94228@gmail.com>
2018-09-17 16:26:32 +0800

    :green_heart: (codeclimate) refactor Header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,32 +1,30 @@
 import { Menu, Icon } from 'antd'
 import Link from 'umi/link'
 
-const Header = ({location}) => (
-  <Menu selectedKeys={[location.pathname]} mode="horizontal" style={{ overflow: "hidden" }}>
-    <Menu.Item key="/">
-      <Link to="/">
-        <Icon type="home" />Home
-      </Link>
-    </Menu.Item>
-    <Menu.Item key="/dashboard">
-      <Link to="/dashboard">
-        <Icon type="laptop" />Dashboard
-      </Link>
-    </Menu.Item>
-    <Menu.Item key="/about">
-      <Link to="/about">
-        <Icon type="appstore-o" />About
-      </Link>
-    </Menu.Item>
-    <Menu.Item key="github">
-      <a
-        href="https://github.com/Raincal/smms"
-        target="_blank"
-        rel="noopener noreferrer">
-        <Icon type="code-o" />
-        Source Code
-      </a>
-    </Menu.Item>
+const MENUS = [
+  { path: '/', icon: 'home', text: 'Home' },
+  { path: '/dashboard', icon: 'laptop', text: 'Dashboard' },
+  { path: '/about', icon: 'appstore-o', text: 'About' },
+  { path: '/github', icon: 'code-o', text: 'Source Code' },
+]
+
+const Header = ({ location }) => (
+  <Menu selectedKeys={[location.pathname]} mode="horizontal" style={{ overflow: 'hidden' }}>
+    {MENUS.map(menu => (
+      <Menu.Item key={menu.path}>
+        {menu.path === '/github' ? (
+          <a href="https://github.com/Raincal/smms" target="_blank" rel="noopener noreferrer">
+            <Icon type={menu.icon} />
+            {menu.text}
+          </a>
+        ) : (
+          <Link to={menu.path}>
+            <Icon type={menu.icon} />
+            {menu.text}
+          </Link>
+        )}
+      </Menu.Item>
+    ))}
   </Menu>
 )
 
