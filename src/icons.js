2aa3217ce89b91ed57fa27c647e8c202179225d0
Raincal <cyj94228@gmail.com>
2018-09-18 13:05:18 +0800

    :bug: (icons) add missing icons

--- src/icons.js
+++ src/icons.js
@@ -1,6 +1,10 @@
 //https://github.com/ant-design/ant-design/issues/12011#issuecomment-420038579
 export { default as AppstoreOutline } from '@ant-design/icons/lib/outline/AppstoreOutline'
+export { default as CheckOutline } from '@ant-design/icons/lib/outline/CheckOutline'
+export { default as CopyOutline } from '@ant-design/icons/lib/outline/CopyOutline'
 export { default as DashboardOutline } from '@ant-design/icons/lib/outline/DashboardOutline'
+export { default as FrownOutline } from '@ant-design/icons/lib/outline/FrownOutline'
 export { default as GithubOutline } from '@ant-design/icons/lib/outline/GithubOutline'
 export { default as HomeOutline } from '@ant-design/icons/lib/outline/HomeOutline'
 export { default as InboxOutline } from '@ant-design/icons/lib/outline/InboxOutline'
+export { default as LinkOutline } from '@ant-design/icons/lib/outline/LinkOutline'
